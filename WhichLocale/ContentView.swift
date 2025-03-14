681403743a8ff2f30b2803b8daeb29d8584f338d
chicio <fabrizio.duroni@gmail.com>
2025-03-14 22:08:15 +0100

    Initial Commit

---
+++ WhichLocale/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhichLocale
+//
+//  Created by Fabrizio Duroni on 14/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhichLocale/WhichLocaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhichLocaleApp.swift
+//  WhichLocale
+//
+//  Created by Fabrizio Duroni on 14/03/25.
+//
+
+import SwiftUI
+
+@main
+struct WhichLocaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
